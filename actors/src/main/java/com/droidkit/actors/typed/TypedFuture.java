5b23cad5adfc79ed294ca73a2d4514db7238a08f
Stepan Korshakov <me@ex3ndr.com>
2014-10-08 22:29:20 +0400

    Fixed TypedFuture

--- actors/src/main/java/com/droidkit/actors/typed/TypedFuture.java
+++ actors/src/main/java/com/droidkit/actors/typed/TypedFuture.java
@@ -5,7 +5,7 @@ import com.droidkit.actors.concurrency.Future;
 /**
  * Created by ex3ndr on 14.09.14.
  */
-class TypedFuture<T> extends Future<T> {
+public class TypedFuture<T> extends Future<T> {
     public void doComplete(T res) {
         onCompleted(res);
     }
